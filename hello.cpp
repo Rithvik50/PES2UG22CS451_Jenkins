593ade993e0ab2102f5cf5315ae3902b184baa78
Rithvik Muthyalapati <109793607+Rithvik50@users.noreply.github.com>
2025-03-13 19:31:50 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, Rithvik\n";
+  cout << "Hello, Everyone\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
